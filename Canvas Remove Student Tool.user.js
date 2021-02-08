059112e41b64e6961d7d0f0aa36a374c21c29596
Nick Meyer <nmeyer5435@gmail.com>
2021-02-08 09:40:26 -0600

    Update Canvas Remove Student Tool.user.js

--- Canvas Remove Student Tool.user.js
+++ Canvas Remove Student Tool.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Canvas Student Course Enrollment Manager
 // @namespace    https://github.com/sukotsuchido/CanvasUserScripts
-// @version      1.5
+// @version      1.5.1
 // @description  A Canvas UserScript to manage course enrollments
-// @author       Chad Scott (ChadScott@katyisd.org)
-// @include     https://*.instructure.com/courses/*/users
-// @require     https://code.jquery.com/jquery-3.4.1.min.js
+// @author       Chad Scott (ChadScott@katyisd.org), Nick Meyer (nmeyer5435@gmail.com)
+// @include     https://canvas.unl.edu/courses/*/users
+// @require     https://code.jquery.com/jquery-3.5.1.min.js
 // @grant        none
 // ==/UserScript==
 (function() {
